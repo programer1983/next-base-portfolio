57d0ca2ded0dcb7f99ef213152298f34c1b3f1d9
Prodi <programer972@gmail.com>
2025-06-10 18:55:51 +0200

    fixed modile styles

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import "./Hero.scss";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Hero = () => {
+  useEffect(() => {
+    if (window.innerWidth >= 500) {
+      AOS.init({ duration: 1000 });
+    } else {
+      const elements = document.querySelectorAll("[data-aos]");
+      elements.forEach((el) => {
+        el.removeAttribute("data-aos");
+        el.removeAttribute("data-aos-delay");
+      });
+    }
+  }, []);
   return (
     <section className="hero" id="home">
       <div className="container">
